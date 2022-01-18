2ed61e3327e3c87c16b3a9cdcf927e4c6e15298e
Davi-Marques <93229005+davi-marques@users.noreply.github.com>
2022-01-18 15:47:39 -0300

    coments

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function tabuada() {
     let num = document.getElementById('num')
     let tab = document.getElementById('seltab')
-    if (num.value.length == 0) {
+    if (num.value.length == 0) { // Se o usuário não digitar nada, ira aparecer uma msg
         alert('Por favor, digite um número!')
     } else {
         let n = Number(num.value)
         let c = 1
-        tab.innerHTML = ''
+        tab.innerHTML = '' // Para limpar a tabuada a cada vez que digitar um número novo
         while (c <= 10) {
-            let item = document.createElement('option')
+            let item = document.createElement('option') // Cria os options dentro do select
             item.text = `${n} x ${c} = ${n*c}`
-            tab.appendChild(item)
+            tab.appendChild(item) // Mostra o calculo :)
             c++
         }
     }
